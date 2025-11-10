1ea5ee0c349fa4c642a66d162e22c0e66f74d68c
javakarprools <javakarprocols@gmail.com>
2025-11-10 11:15:31 +0530

    Final-Frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,16 +5,18 @@ import Sports from './pages/user/home/sports.jsx';
 import OurTeam from './pages/user/home/OurTeam.jsx';
 import RandomLogoSlider from './pages/user/home/Logo_Section.jsx';
 import LogoSlider1 from './pages/user/home/Logo_Section1.jsx';
+import BigCatTwister from './pages/user/home/BigCatTwister.jsx';
 
 const App = () => {
   return (
     <>
     <CardsWithArrows />
     <BigCatStack />
-    {/* <Sports /> */}
     <OurTeam />
     <RandomLogoSlider />
     <LogoSlider1 />
+    {/* <BigCatTwister /> */}
+    <Sports />
     </>
   )
 }
