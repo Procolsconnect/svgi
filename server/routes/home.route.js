063d8b9c066bd97045f95792af5cfb934c59b4e6
javakarprools <javakarprocols@gmail.com>
2025-11-11 16:28:01 +0530

    Service Offering Backend

--- server/routes/home.route.js
+++ server/routes/home.route.js
@@ -1,4 +1,5 @@
 const home = require("../controllers/home/HomeHeroController");
+const service = require("../controllers/home/ServiceOfferingsController");
 const upload = require("../middleware/multer");
 const {validate} = require("../middleware/validate");
 const {createHeroSchema, updateHeroSchema} = require("../inputvalidations/home.validation");
@@ -10,6 +11,11 @@ module.exports = function homeRoutes(app) {
   app.get("/api/hero",home.getHeroController);
   app.put("/api/hero/:id",upload.single("media"),validate(updateHeroSchema),home.updateHeroController);
   app.delete("/api/hero/:id",home.deleteHeroController);
+
+  app.get("/api/service-offerings",service.getServiceOfferingsController);
+  app.post("/api/service-offerings",upload.single("image"),service.createServiceOfferingController);
+  app.put("/api/service-offerings/:id",upload.single("image"),service.updateServiceOfferingController);
+  app.delete("/api/service-offerings/:id",service.deleteServiceOfferingController);
 
 
 
