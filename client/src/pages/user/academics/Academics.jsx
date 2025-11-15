82867d5f34b1f853101f40b7f82540da12e641c0
javakarprools <javakarprocols@gmail.com>
2025-11-16 03:19:33 +0530

    AcademicsCourseOutcome Frontend

--- client/src/pages/user/academics/Academics.jsx
+++ client/src/pages/user/academics/Academics.jsx
@@ -1,13 +1,14 @@
 import Overview from './Overview'
 import { Routes, Route } from "react-router-dom";
+import CourseOutcome from "./CourseOutcome"
 const Admissions = () => {
   return (
     <div>
 
       <Routes>
         <Route index element={<Overview />} />
-        {/* <Route path="ug" element={<Ug />} />
-        <Route path="pg" element={<Pg />} />
+        <Route path="courseoutcome" element={<CourseOutcome />} />
+        {/* <Route path="pg" element={<Pg />} />
         <Route path="procedure" element={<Procedure />} />
         <Route path="research" element={<Research />} /> */}
       </Routes>
